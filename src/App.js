1e8b0ba77853874ee05361798d65f55193378553
craigjtwigg <81586983+craigjtwigg@users.noreply.github.com>
2021-11-21 21:25:01 +0000

    form added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Skull from './assets/kingblck-skull.webp';
 import './App.css';
 import React from 'react';
+import { Form } from './Form';
 
 function App() {
   return (
     <div className="App">
       <div className="content">
         <img src={Skull} alt="King Blck logo" />
+        <Form/>
       </div>
     </div>
   );
--- src/Form.js
+++ src/Form.js
@@ -1,10 +1,44 @@
 import React from 'react'
 
-export default function Form() {
-    return (
-<form>
-        <input placeholder="youremail@kingblck.com" type="text"></input>
-        <button>JOIN US</button>
+ const encode = (data) => {
+    return Object.keys(data)
+        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+        .join("&");
+  }
+
+  export class Form extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { email: ""};
+    }
+
+    /* Here’s the juicy bit for posting the form submission */
+
+    handleSubmit = e => {
+      fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: encode({ "form-name": "contact", ...this.state })
+      })
+        .then(() => alert("Success!"))
+        .catch(error => alert(error));
+
+      e.preventDefault();
+    };
+
+    handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
+    render() {
+      const { email } = this.state;
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <p>
+              <input placeholder="youremail@kingblck.com" type="email" name="email" value={email} onChange={this.handleChange} />
+          </p>
+          <p>
+            <button type="submit">s u b m i t</button>
+          </p>
         </form>
-    )
-}
+      );
+    }
+  }
