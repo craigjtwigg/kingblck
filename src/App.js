e36b01321b0b0553ac9a115c7d090bea66f0ae1c
craigjtwigg <81586983+craigjtwigg@users.noreply.github.com>
2021-11-21 20:49:18 +0000

    background switched to .gif, skull switched to .webp

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-import BackgroundMP4 from './assets/kingblck-background.mp4';
-import BackgroundWebM from './assets/kingblck-background.webm';
-import Skull from './assets/kingblck-skull.png';
-//import TextField from '@mui/material';
+import Skull from './assets/kingblck-skull.webp';
 import './App.css';
 import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <video playsInline preload="auto" muted autoPlay loop id="background-video">
-        <source src={BackgroundWebM} type="video/webm" />
-        <source src={BackgroundMP4} type="video/mp4" />
-      </video>
       <div className="content">
         <img src={Skull} alt="King Blck logo" />
       </div>
